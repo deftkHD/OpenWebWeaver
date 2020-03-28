7afde03ed80cb6b4e76ecc62087ff1906061464e
finn <32391033+deftkHD@users.noreply.github.com>
2020-03-28 17:53:24 +0100

    fixed task start and end date

--- src/de/deftk/lonet/api/model/feature/Task.kt
+++ src/de/deftk/lonet/api/model/feature/Task.kt
@@ -12,13 +12,14 @@ import javax.net.ssl.HttpsURLConnection
 
 class Task(jsonObject: JsonObject, val group: Member) {
 
+    // no need to worry, session id is already expired ^^
     // curl 'https://www.lo-net2.de/wws/130837.php?sid=49257099026314249558508520868961073741828331S52b413cc' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: de,en-US;q=0.7,en;q=0.3' --compressed -H 'Referer: https://www.lo-net2.de/wws/130837.php?tid=235266&sid=49257099026314249558508520858201073741828331Sd37c352f&enableautogrow=1' -H 'Content-Type: multipart/form-data; boundary=---------------------------18234854469591812993786830883' -H 'Origin: https://www.lo-net2.de' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Cookie: wwspc=1; wwsc=999701104119441160' -H 'Upgrade-Insecure-Requests: 1' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data-binary $'-----------------------------18234854469591812993786830883\r\nContent-Disposition: form-data; name="tid"\r\n\r\n235266\r\n-----------------------------18234854469591812993786830883\r\nContent-Disposition: form-data; name="complete"\r\n\r\n0\r\n-----------------------------18234854469591812993786830883\r\nContent-Disposition: form-data; name="set"\r\n\r\nAls unerledigt markieren\r\n-----------------------------18234854469591812993786830883--\r\n'
 
     val id: String = jsonObject.get("id").asString
     val title: String? = jsonObject.get("title")?.asString
     val description: String? = jsonObject.get("description")?.asString
-    val startDate: Date = Date(jsonObject.get("start_date").asLong * 1000)
-    val endDate: Date? = if (jsonObject.has("due_date")) Date(jsonObject.get("due_date").asLong * 1000) else null
+    val startDate: Date? = if(jsonObject.get("start_date").asInt != 0) Date(jsonObject.get("start_date").asLong * 1000) else null
+    val endDate: Date? = if (jsonObject.get("due_date").asInt != 0) Date(jsonObject.get("due_date").asLong * 1000) else null
     var completed: Boolean = jsonObject.get("completed").asInt == 1
         private set
     val creationDate: Date
