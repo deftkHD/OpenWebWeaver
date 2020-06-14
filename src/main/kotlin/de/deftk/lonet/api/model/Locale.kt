e81d278b7e811e9b759cf22509338d1d6091b325
finn <32391033+deftkHD@users.noreply.github.com>
2020-06-14 16:50:59 +0200

    allow custom params for autologin url request

---
+++ src/main/kotlin/de/deftk/lonet/api/model/Locale.kt
@@ -0,0 +1,10 @@
+package de.deftk.lonet.api.model
+
+enum class Locale(val code: String) {
+    ENGLISH("en"),
+    GERMAN("de"),
+    FRANCE("fr"),
+    SPANISH("es"),
+    ITALIAN("it"),
+    TURKEY("tr")
+}
--- src/main/kotlin/de/deftk/lonet/api/request/UserApiRequest.kt
+++ src/main/kotlin/de/deftk/lonet/api/request/UserApiRequest.kt
@@ -1,16 +1,28 @@
 package de.deftk.lonet.api.request
 
+import com.google.gson.JsonObject
 import de.deftk.lonet.api.model.Feature
+import de.deftk.lonet.api.model.Locale
 import de.deftk.lonet.api.model.User
 import java.io.Serializable
 
 class UserApiRequest(private val user: User) : OperatorApiRequest(user), Serializable {
 
-    fun addGetAutoLoginUrlRequest(): List<Int> {
+    fun addGetAutoLoginUrlRequest(disableLogout: Boolean? = null, disableReceptionOfQuickMessages: Boolean? = null, enslaveSession: Boolean? = null, locale: Locale? = null, pingMaster: Boolean? = null, sessionTimeout: Int? = null, targetData: Any? = null, targetIframes: Boolean? = null, targetUrlPath: String? = null): List<Int> {
         ensureCapacity(2)
+        val requestProperties = JsonObject()
+        if (disableLogout != null) requestProperties.addProperty("disable_logout", disableLogout)
+        if (disableReceptionOfQuickMessages != null) requestProperties.addProperty("disable_reception_of_quick_messages", disableReceptionOfQuickMessages)
+        if (enslaveSession != null) requestProperties.addProperty("enslave_session", enslaveSession)
+        if (locale != null) requestProperties.addProperty("locale", locale.code)
+        if (pingMaster != null) requestProperties.addProperty("ping_master", pingMaster)
+        if (sessionTimeout != null) requestProperties.addProperty("session_timeout", sessionTimeout)
+        if (targetData != null) TODO("don't know how to handle mixed data")
+        if (targetIframes != null) requestProperties.addProperty("target_iframes", targetIframes)
+        if (targetUrlPath != null) requestProperties.addProperty("target_url_path", targetUrlPath)
         return listOf(
                 addSetFocusRequest("trusts", user.getLogin()),
-                addRequest("get_url_for_autologin", null) //TODO this method allows parameters
+                addRequest("get_url_for_autologin", requestProperties)
         )
     }
 
