345f8ba00487f54ef99e52a4dfe91b6e714fa9b0
deftkHD <32391033+deftkHD@users.noreply.github.com>
2020-12-06 00:41:56 +0100

    made request handler serializable

--- src/main/kotlin/de/deftk/lonet/api/request/handler/IRequestHandler.kt
+++ src/main/kotlin/de/deftk/lonet/api/request/handler/IRequestHandler.kt
@@ -3,8 +3,9 @@ package de.deftk.lonet.api.request.handler
 import de.deftk.lonet.api.model.abstract.IContext
 import de.deftk.lonet.api.request.ApiRequest
 import de.deftk.lonet.api.response.ApiResponse
+import java.io.Serializable
 
-interface IRequestHandler {
+interface IRequestHandler: Serializable {
 
     fun performRequest(request: ApiRequest, context: IContext): ApiResponse
 
